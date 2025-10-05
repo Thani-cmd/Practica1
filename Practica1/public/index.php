3ced53d426c2c18847ed41737801b0a7f4f3d356
Thani-cmd <thani.martinez2@gmail.com>
2025-10-05 14:39:56 +0100

    Add category page to display animals by category

--- Practica1/public/index.php
+++ Practica1/public/index.php
@@ -1,6 +1,19 @@
 <?php
     include(".../includes/header.php");
+    include('datos.php');
 ?>
+
+    <main>
+        <h1>Listado de Caterogías</h1>
+        <ul>
+            <?php
+                foreach($categorias as $categoria){
+                    echo '<li><a href="category.php?id=' . $categoria['id'] . '">' . $categoria['nombre'] . '</a></li>';
+                }
+            ?>
+        </ul>
+    </main>
+
 
 <?php
     include(".../includes/footer.php");
